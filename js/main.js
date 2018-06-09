5d5e7578248625d036cdc32a91b9b83b60e6c917
Marcus Virginia <marcusvirg@yahoo.com>
2018-06-09 13:47:05 -0500

    removed console statements and added favicon

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ var modalCloseBtn = document.getElementById("btn-close")
 blogBtn.addEventListener("click", openModal);
 modalCloseBtn.addEventListener("click", closeModal)
 
-console.log(modal)
-console.log(blogBtn)
-
 function openModal() {
   modal.classList.add("is-active")
   console.log(modal)
